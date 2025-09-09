dac677a0a5f7f151bb29904089c6ad6565674fb7
ariekas <ariariekas@gmail.com>
2025-09-09 23:19:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const input = require('./lib/generateDate')
+const inputManual = require('./lib/converdManual')
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+rl.question("Masukkan tanggal (format: DD-MM-YYYY): ", (inputDate) => {
+  input.converdDate(inputDate)
+  // inputManual.generateDateManual(inputDate)
+  rl.close()
+})
